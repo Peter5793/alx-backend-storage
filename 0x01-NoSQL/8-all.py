14d34a9e5e4302be1b2d8240c097115605922fbc
Peter Lugalia <masaba.peter68@gmail.com>
2021-08-03 20:28:08 +0000

    add python list function

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" list all documents in a collections"""
+import pymongo
+
+
+def list_all(mongo_collection) -> list:
+    """ function that list all the documents
+    """
+    documents: list = []
+    for i in mongo_collection.find():
+      documents.append(i)
+
+    return i
